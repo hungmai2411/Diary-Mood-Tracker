7f28ec945f6feaf74cb66e10d289d55472579b2a
James2109 <20520292@gm.uit.edu.vn>
2023-01-27 18:03:17 +0700

    Create category.g.dart

---
+++ lib/features/category/models/category.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'category.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class CategoryAdapter extends TypeAdapter<Category> {
+  @override
+  final int typeId = 4;
+
+  @override
+  Category read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Category(
+      title: fields[0] as String,
+      content: fields[1] as String,
+      key: fields[2] as int?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Category obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.content)
+      ..writeByte(2)
+      ..write(obj.key);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CategoryAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
